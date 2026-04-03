e19398fc6d98e0f5306605b2406988fa739ff12d
Mubeen <mubeen@localhost.localdomain>
2026-04-03 11:41:56 +0530

    Updated TrustRegistry.sol code, so that only authorized entities can anchor the document

--- blockchain/src/TrustRegistry.sol
+++ blockchain/src/TrustRegistry.sol
@@ -2,13 +2,28 @@
 pragma solidity ^0.8.20;
 
 contract TrustRegistry {
-    mapping(string => bytes32) private documentHashes;
+    address public governmentOfficial; // The only account allowed to "seal" docs
 
-    function anchorDocument(string memory did, bytes32 hash) public {
-        documentHashes[did] = hash;
+    mapping(string => bytes32) public verifiedDocuments;
+    event DocumentAnchored(string indexed did, bytes32 documentHash);
+
+    // This runs once when the contract is deployed
+    constructor() {
+        governmentOfficial = msg.sender; 
     }
 
-    function getDocumentHash(string memory did) public view returns (bytes32) {
-        return documentHashes[did];
+    // A "Modifier" is a guard that checks a condition before running a function
+    modifier onlyGovernment() {
+        require(msg.sender == governmentOfficial, "Not authorized: Only Govt can anchor.");
+        _;
+    }
+
+    function anchorDocument(string memory _did, bytes32 _docHash) public onlyGovernment {
+        verifiedDocuments[_did] = _docHash;
+        emit DocumentAnchored(_did, _docHash);
+    }
+
+    function getDocumentHash(string memory _did) public view returns (bytes32) {
+        return verifiedDocuments[_did];
     }
 }
